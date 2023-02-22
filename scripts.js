f1c1e9020ba732e34051e595251c043a9f0d58a3
GabeSilvaDev <gabriel.silva@contatodev.com.br>
2023-02-22 03:57:25 -0300

    Upando Projeto.

---
+++ scripts.js
@@ -0,0 +1,27 @@
+import { ImageService } from "./app/services/imageService.js";
+import { Game } from "./app/game.js";
+
+const listElement = document.querySelector('.card-list');
+const resetButton = document.querySelector('#reset-button');
+const logoList = [
+    'angular',
+    'react',
+    'vue'
+];
+
+ImageService.loadImageAll([
+    './img/treinaweb.svg',
+    logoList.map(logo => `./img/${logo}.svg`)
+]).then(() => {
+    listElement.addEventListener('click', onCardClick);
+    resetButton.addEventListener('click', () => Game.start(logoList));
+    Game.start(logoList);
+})
+
+function onCardClick({target}) {
+    const listItemElement = target.closes('.flipper-contaainer');
+    if(listItemElement){
+        const index = [...listElement.children].indexOf(listElement);
+        Game.selectedCard(index);
+    }
+}
